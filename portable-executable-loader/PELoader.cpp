eda860d2b0aef892bd9993e8ca5f4d250cbcab6e
Liron Danan <lirondanan12@gmail.com>
2026-09-16 22:59:50 +0300

    load the base of the image to virtual memory

--- portable-executable-loader/PELoader.cpp
+++ portable-executable-loader/PELoader.cpp
@@ -1,1 +1,37 @@
 #include "PELoader.h"
+#include <Windows.h>
+
+
+
+HMODULE PELoader::loadLibrary(std::vector<std::byte> dllBuffer) {
+	if (dllBuffer.size() < sizeof(PIMAGE_DOS_HEADER)) {
+		return NULL;
+	}
+
+	std::byte* libraryPtr = dllBuffer.data();
+
+	PIMAGE_DOS_HEADER imageDosHeader = (PIMAGE_DOS_HEADER)libraryPtr;
+	if (IMAGE_DOS_SIGNATURE != imageDosHeader->e_magic) { return NULL; }
+
+	PIMAGE_NT_HEADERS imageNtHeaders = (PIMAGE_NT_HEADERS)((std::byte*)libraryPtr + imageDosHeader->e_lfanew);
+	if (IMAGE_NT_SIGNATURE != imageNtHeaders->Signature) { return NULL; }
+
+	LPVOID virtualImage = VirtualAlloc(
+		NULL,
+		imageNtHeaders->OptionalHeader.SizeOfImage,
+		MEM_COMMIT | MEM_RESERVE,
+		PAGE_READWRITE
+	);
+
+	if (virtualImage == NULL) { return NULL; }
+
+	std::memcpy(virtualImage, libraryPtr, imageNtHeaders->OptionalHeader.SizeOfHeaders);
+
+	return (HMODULE)virtualImage;
+}
+
+void PELoader::freeLibrary(HMODULE loadAddress) {
+	if (loadAddress != NULL) {
+		VirtualFree(loadAddress, 0, MEM_RELEASE);
+	}
+}
--- portable-executable-loader/main.cpp
+++ portable-executable-loader/main.cpp
@@ -1,4 +1,26 @@
 
+#include <fstream>
+#include <iostream>
+
+#include "PELoader.h"
+
 int main() {
+	PELoader loader = PELoader();
+	std::vector<std::byte> dllBuffer = std::vector<std::byte>();
+
+	std::ifstream file("C:\\Projects\\Dll1.dll", std::ios::binary);
+
+	char fileByte = 0;
+	while (file.get(fileByte)) {
+		dllBuffer.push_back((std::byte)fileByte);
+	}
+	HMODULE test = loader.loadLibrary(dllBuffer);
+
+	std::cout << "test: " << test << std::endl;
+	PIMAGE_DOS_HEADER imageDosHeader = (PIMAGE_DOS_HEADER)test;
+	std::cout << "test2: " << imageDosHeader->e_magic;
+
+	loader.freeLibrary(test);
+
 	return 0;
 }
